cb874e6dec2baf9bc76d11cbcab4042950984f54
Pratham Rajbhoj <84040204+Pratham2301@users.noreply.github.com>
2022-06-04 23:20:12 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,19 +1,14 @@
-my_email = "rajbhojpr@rknec.edu"
-my_password = "Pratham23"
+my_email = ""
+my_password = ""
 
-my_to_list = ['rajbhojpratham@gmail.com', 'prathamrajbhoj2003@gmail.com', 'dhakatesr_1@rknec.edu']
+my_to_list = ['','','']
 
 my_subject = "Python Automated Email"
 
 my_body = '''
 Hello Pratham, 
 
-This is auto Generated mail-3 for testing purpose
+This is auto Generated mail for testing purpose
 
 Thank You!!!
 '''
-
-
-# my_email = "rajbhojpratham@gmail.com"
-# my_password = "Pratham2301"
-# to_list = ['rajbhojpr@rknec.edu', 'prathamrajbhoj2003@gmail.com']
