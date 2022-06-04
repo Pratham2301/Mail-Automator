dcbde068b131eed919c5f1372e9c3e377ee3d060
Pratham Rajbhoj <84040204+Pratham2301@users.noreply.github.com>
2022-06-04 23:19:18 +0530

    Update mailsender.py
    minor changes

--- mailsender.py
+++ mailsender.py
@@ -21,63 +21,13 @@ msg['Subject'] = subject_mail
 
 body = '''
 <html>
-
 <head>
-    <title>RCOEM WEBINAR</title>
+    <title></title>
 </head>
 <style>
-    h1 {
-        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-        color: #f7f8f9;
-    }
-
-    .box {
-        background-color: #384a5b;
-        width: 100%;
-        color: white;
-        padding: 5%;
-        border-radius: 20px;
-    }
-
-    body {
-
-        margin-top: 5%;
-        padding-left: 15%;
-        padding-right: 15%;
-        color: var(--primary-color);
-        background-color: #212325;
-        background: linear-gradient(45deg, #394754, #212325);
-        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    }
 </style>
 
 <body>
-    <div class="box">
-        <h1 align="center">REMINDER</h1>
-    Hello Enthusiast,
-        <p class="content"> We are glad that you registered for the seminar on <strong>Blockchain</strong> brought to you by The
-        <strong>Codebreakers x Lumos Labs.</strong>
-        </p>
-        <p class="list">
-            Here are some important instructions about the seminar.<br><br>
-            1. You are requested to carry your laptop as the session will be hands-on.<br>
-            2. Due to heavy rush, the seating arrangements will be on a first come first serve basis.<br>
-            3. We recommend you to go through the prerequisites and agenda (attached below) of the seminar for better outcomes.<br><br>
-
-            <strong> Date: 04/06/22 </strong><br>
-            <strong> Time: 1.00 p.m </strong><br><br>
-            Please note the change in the venue.<br><br>
-            The seminar will be conducted in the <strong>MBA Auditorium,RCOEM</strong> .<br><br>
-        </p>
-        <p >
-            Regards,<br>
-            Inshal Khan<br>
-            President (The Codebreakers Club,RCOEM)<br>
-        </p>
-
-
-    </div>
-
 </body>
 
 </html>
